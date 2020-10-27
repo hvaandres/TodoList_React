5b3494b2b351c224b610588def2f266485195978
Andres Haro <hvaandres@icloud.com>
2020-10-27 13:59:00 -0600

    bug fixed

--- todo_list_v2/src/App.js
+++ todo_list_v2/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <TodoForm />
+      <TodoList />
      <h1>To-Do App</h1>
     </div>
   );
