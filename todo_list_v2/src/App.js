450805f6edffa52dd7d9501029d083970fc5294c
Andres Haro <hvaandres@icloud.com>
2020-10-27 00:15:23 -0600

    first todo list react

---
+++ todo_list_v2/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoForm />
+     <h1>To-Do App</h1>
+    </div>
+  );
+}
+
+export default App;
