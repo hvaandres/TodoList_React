9ca60dea277e8cc55227b3b481d4c8e1db15a2fa
Andres Haro <hvaandres@icloud.com>
2020-11-04 21:32:43 -0700

    update

--- todo_list_v2/src/App.js
+++ todo_list_v2/src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
       <TodoList />
-     <h1>To-Do App</h1>
+      <h1>To-Do App</h1>
     </div>
   );
 }
